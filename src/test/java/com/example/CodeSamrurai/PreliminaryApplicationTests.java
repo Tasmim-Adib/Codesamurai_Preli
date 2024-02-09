1f11fe0973c2cbb6333a1a7c5f5d3d1d5677d157
Tasmim-Adib <tasmim.adib@gmail.com>
2024-02-09 16:10:00 +0600

    first Commit

---
+++ src/test/java/com/example/CodeSamrurai/PreliminaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeSamrurai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreliminaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
